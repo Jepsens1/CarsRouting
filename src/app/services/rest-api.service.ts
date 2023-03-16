84ea2785af46586c205d1890edad6db2bc2636c8
Jepsens1 <jepsens123@outlook.dk>
2023-03-16 12:54:32 +0100

    commit

---
+++ src/app/services/rest-api.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Car } from '../interfaces/car';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestAPIService {
+  url: string = "https://localhost:7263"
+  constructor(private http: HttpClient) { }
+
+  getAllCars(): Observable<Car[]> {
+    return this.http.get<Car[]>(this.url+'/Cars/GetAll');
+  }
+  getCar(model: string): Observable<Car> {
+    const url = this.url + `/Cars/Get?model=${model}`;
+    return this.http.get<Car>(url);
+  }
+  deleteCar(model: string): Observable<string> {
+    const url = this.url + `/Cars/Delete?model=${model}`;
+    return this.http.delete<string>(url);
+  }
+  addCar(rankform: number, modelform:string, quantityform: number, changeQuantityPercentform: number) : Observable<string>
+  {
+    const url = this.url + `/Cars/Add?rank=${rankform}&model=${modelform}&quantity=${quantityform}&changeQuantity=${changeQuantityPercentform}`;
+    return this.http.post<string>(url,'');
+  }
+  editCar(rankform: number, modelform:string, quantityform: number, changeQuantityPercentform: number) : Observable<string>
+  {
+    const url = this.url + `Update?rank=${rankform}&model=${modelform}&quantity=${quantityform}&changeQuantity=${changeQuantityPercentform}`;
+    return this.http.post<string>(url, '');
+  }
+}
